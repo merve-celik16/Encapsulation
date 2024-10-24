64ba180a94318595f31ddd5bfc0bf0d0e1ef0d55
merve-celik16 <mervedursunn34@gmail.com>
2024-10-24 23:14:12 +0300

    Proje dosyası ekle.

---
+++ Araba.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulation
+{
+    public class Araba
+    {
+        public string marka { get; set; } 
+        public string model { get; set; }
+        public string renk { get; set; }
+
+        private int kapiSayisi; // private ile kapsülleme yapıldı.
+
+        
+        public int Kapisayisi // Kapsülleme yapıldığı için get ve set metotu kullanıldı
+        {
+            get
+            {
+                return kapiSayisi;
+
+            }
+            set
+            {
+                
+                if (value == 2 || value == 4) // Girilen değer 2 veya 4 e eşit ise kapısı sayısını atayacak
+                {
+                    kapiSayisi = value;
+                }
+                else // girilen kapı sayısı 2 veya 4 değil ise -1 olacak
+                {
+                    Console.WriteLine("Bir arabanın kapı sayısı 2 veya 4 olabilir. Kapı sayısı -1 olarak ayarlandı.");
+                    kapiSayisi = -1;
+                }
+            }
+
+        }
+        public Araba(string _marka, string _model, string _renk, int _kapisayisi) // Parametreli yapıcı metot tanımlanmıştır
+        {
+            marka = _marka;
+            model = _model; 
+            renk = _renk;
+            Kapisayisi= _kapisayisi;
+        }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Encapsulation
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Araba a1 = new Araba("Skoda","Fabia","Mavi",4); // a1 adında nesne oluşmuştur
+            
+            Araba a2 = new Araba("Renault","Clio","Turunucu",5); // a2 adında nesne oluşmuştur.
+           
+            Console.WriteLine("Araba 1: " );
+            Console.WriteLine( a1.marka + " " + a1.model + ", Renk: " + a1.renk + ", Kapı Sayısı: " + a1.Kapisayisi);
+            Console.WriteLine("Araba 2: " );
+            Console.WriteLine(a2.marka + " " + a2.model + ", Renk: " + a2.renk + ", Kapı Sayısı: " + a2.Kapisayisi);
+        }
+
+    }
+    
+}
